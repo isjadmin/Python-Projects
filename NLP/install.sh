d256c229e19078758efa76812bcf088c52cd356a
PrajktaMangulkar <prajkta.mangulkar@smarthub.ai>
2022-11-30 14:08:56 +0530

    renamed files

--- NLP/install.sh
+++
@@ -1,8 +0,0 @@
-mkdir "/home/ubuntu/python_files/file_sorting"
-mkdir "/home/ubuntu/Resume_Downloads"
-
-cp "/home/ubuntu/python_files/file_sorting/service_bash" "/etc/init.d/service_bash"
-
-chmod +x /etc/init.d/service_bash
-
-service service_bash start
